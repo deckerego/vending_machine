e378b40a9324e751d2567533b84024e2703b6888
John Ellis <532789+deckerego@users.noreply.github.com>
2022-10-17 21:08:33 -0400

    Flame animation on button press

--- arduino/dragonlights/dragonlights.ino
+++ arduino/dragonlights/dragonlights.ino
@@ -1,31 +1,49 @@
 #define GREENPIN  9
 #define BLUEPIN   10
 #define REDPIN    11
-#define WAITTIME  1000
+#define BUTTON    12
+
+int buttonState = 0;
 
 void setup() {
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN, OUTPUT);
   pinMode(REDPIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-}
+  pinMode(BUTTON, INPUT_PULLUP);
 
-void loop() {
   analogWrite(GREENPIN, 0);
   analogWrite(BLUEPIN, 0);
   analogWrite(REDPIN, 0);
+}
+
+void loop() {
+  if(digitalRead(BUTTON) == LOW) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    for (int i = 0; i <= 255; i += 16) {
+      analogWrite(REDPIN, i);
+      analogWrite(BLUEPIN, i / 2);
+      analogWrite(GREENPIN, i / 2);
+      delay(100);
+    }
+
+    for (int i = 255; i >= 0; i -= 8) {
+      analogWrite(REDPIN, 255);
+      analogWrite(BLUEPIN, i / 2);
+      analogWrite(GREENPIN, i / 2);
+      delay(100);
+    }
+
+    delay(5000);
+
+    for (int i = 255; i >= 0; i -= 8) {
+      analogWrite(REDPIN, i);
+      delay(100);
+    }
+
+    analogWrite(GREENPIN, 0);
+    analogWrite(BLUEPIN, 0);
+    analogWrite(REDPIN, 0);
+  }
   digitalWrite(LED_BUILTIN, LOW);
-  delay(WAITTIME);
-
-  analogWrite(GREENPIN, 255);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(WAITTIME);
-
-  analogWrite(GREENPIN, 0);
-  analogWrite(BLUEPIN, 255);
-  delay(WAITTIME);
-
-  analogWrite(BLUEPIN, 0);
-  analogWrite(REDPIN, 255);
-  delay(WAITTIME);
 }
